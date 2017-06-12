3587c7ddf9c84c7226de6196a7eddb4b2d59b12a
igel93 <anne@anneigel.com>
2017-06-12 10:34:54 +0200

    updated small fixes on most of the pages, including titles, jquery, making it more resposive and other element

--- hack_saga/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $dbname = 'saga';
-  $dbuser = 'root';
-  $dbpass = 'root';
-  $dbhost = 'localhost:8888'; // localhost should suffice
-  $conn = mysql_connect($dbhost, $dbuser, $dbpass) or exit(mysql_error());
-  mysql_select_db($dbname, $conn) or exit(mysql_error());
-  ?>
